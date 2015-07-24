d8d52e36bd8816d036542fb05867883fe3269902
Pablo Leano <pleasedontbelong@gmail.com>
2015-07-24 17:56:23 +0200

    First Draft for swagger 2.0

--- rest_framework_swagger/urls.py
+++ rest_framework_swagger/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns
 from django.conf.urls import url
-from rest_framework_swagger.views import SwaggerResourcesView, SwaggerApiView, SwaggerUIView
-
+from rest_framework_swagger.views import (SwaggerResourcesView, SwaggerApiView, SwaggerUIView,
+                                          Swagger2JSONView)
 
 urlpatterns = patterns(
     '',
     url(r'^$', SwaggerUIView.as_view(), name="django.swagger.base.view"),
     url(r'^api-docs/$', SwaggerResourcesView.as_view(), name="django.swagger.resources.view"),
-    url(r'^api-docs/(?P<path>.*)/?$', SwaggerApiView.as_view(), name='django.swagger.api.view'),
+    url(r'^swagger\.json$', Swagger2JSONView.as_view(), name='django.swagger.2.0.json.view'),
 )
