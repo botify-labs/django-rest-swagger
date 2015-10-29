2b59e83c4e56e4727cafac113cf0547484f0d739
Pablo Leano <pleasedontbelong@gmail.com>
2015-10-29 18:25:48 +0100

    Fix default value key for parameters

--- rest_framework_swagger/yamlparser.py
+++ rest_framework_swagger/yamlparser.py
@@ -349,8 +349,8 @@ class YAMLDocstringParser(object):
 
             normalize_data_format(data_type, data_format, f)
 
-            if field.get('defaultValue', None) is not None:
-                f['defaultValue'] = field.get('defaultValue', None)
+            if field.get('default', None) is not None:
+                f['default'] = field.get('default', None)
 
             # Allow Multiple Values &f=1,2,3,4
             if field.get('allowMultiple'):
